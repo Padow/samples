24240c06130232c4689b1734eec7fb68ebb20332
doucheme <patrick.douchement@fastconnect.fr>
2017-09-13 13:38:27 +0200

    :3

--- org/alien4cloud/nginx/scripts/nginx/configure.sh
+++ org/alien4cloud/nginx/scripts/nginx/configure.sh
@@ -44,7 +44,7 @@ sudo sed -i -e "s/%LISTEN_PORT%/${LISTEN_PORT}/g" /etc/nginx/sites-enabled/defau
 sudo sed -i -e "s/%SERVER_NAME%/${SERVER_NAME}/g" /etc/nginx/sites-enabled/default
 
 # sudo sed -i -e "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/sysconfig/selinux
-sudo cp /etc/nginx/sites-enabled/default /etc/nginx/conf.d/reverseproxy.conf
+# sudo cp /etc/nginx/sites-enabled/default /etc/nginx/conf.d/reverseproxy.conf
 sudo setsebool -P httpd_can_network_connect true
 # sudo systemctl restart nginx
 
