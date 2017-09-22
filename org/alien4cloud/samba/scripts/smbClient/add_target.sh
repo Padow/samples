0fe90b61800be619ecc66fc0d091059d2eade51e
doucheme <patrick.douchement@fastconnect.fr>
2017-09-22 10:33:35 +0200

    654

--- org/alien4cloud/samba/scripts/smbClient/add_target.sh
+++ org/alien4cloud/samba/scripts/smbClient/add_target.sh
@@ -9,7 +9,7 @@ sudo chmod 777 $MOUNT_POINT
 
 if [[ "$(which yum)" != "" ]]
   then
-  sudo mount -t cifs //$SAMBA_SERVER_IP/$SHARE_NAME $MOUNT_POINT -o --rw,sec=ntlm,guest
+  sudo mount -t cifs //$SAMBA_SERVER_IP/$SHARE_NAME $MOUNT_POINT -o rw,sec=ntlm,guest
 elif [[ "$(which apt-get)" != "" ]]
   then
     sudo mount -t cifs -o //$SAMBA_SERVER_IP/$SHARE_NAME $MOUNT_POINT rw,sec=ntlm,guest
